68a35d562939dade2a6a4c8b3ba9b56d33e466bd
RichardHpaYoobee <richard.hpa@acgedu.com>
2018-08-24 11:49:54 +1200

    added front end search tweet form, tweets gets shown on the index page

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,47 @@
 $(document).ready(function(){
-    console.log("js/jquery is loading");
+
 });
+
+$('#form').submit(function(event){
+    event.preventDefault();
+    $('#tweetCard').empty();
+    var url = 'http://192.168.33.10:5000';
+    var value = $('#search').val();
+    if(value.length > 0){
+        $('#tweetCard').append('<div class="loader"></div>');
+        url += "/search=" + value;
+    } else {
+        alert("please enter a value");
+        return;
+    }
+
+    $.ajax({
+        url: url,
+        dataType: 'json',
+        success:function(tweet){
+            console.log(tweet.statuses[0]);
+            $('#tweetCard').empty();
+            $('#tweetCard').append(
+                '<div class="card flex-row flex-wrap">'+
+                    '<div class="card flex-row flex-wrap">'+
+                        '<div class="card-header border-0">'+
+                            '<img src="'+tweet.statuses[0].user.profile_image_url+'" alt="">'+
+                        '</div>'+
+                    '<div class="card-block px-2">'+
+                        '<h4 class="card-title">'+tweet.statuses[0].user.name+'</h4>'+
+                        '<p class="card-text">'+tweet.statuses[0].text+'</p>'+
+                    '</div>'+
+                    '<div class="card-footer w-100">'+
+                        tweet.statuses[0].created_at+
+                    '</div>'+
+                '</div>'
+            );
+            // console.log(tweet.statuses[0]);
+        },
+        error:function(error){
+            $('#tweetCard').empty()
+            alert("error, look into your console");
+            console.log(error);
+        }
+    });
+})
